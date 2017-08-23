4c7869bae77340bfa03bb4a0835ed5822243bcbb
Vaughn <vwalker04@yahoo.com>
2017-08-22 21:04:25 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,4 +4,10 @@ $(document).ready(function (){
     pause: 3000,
     slideMargin: 20
   });
+
+  $('.about_img').hover(function() {
+    $(this).css("opacity", "1");
+    }, function() {
+    $(this).css("opacity", ".80");
+  });
 });
