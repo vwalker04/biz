2e06f3a70b1fc9383cfc184bf622bc7bf2b5a4f0
Vaughn <vwalker04@yahoo.com>
2017-10-08 02:45:41 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,14 +1,8 @@
 /* Forces user to load page with https if not http */
- if (location.protocol != 'https:') {
-location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+if (location.protocol != 'https:') {
+  location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
 }
 
-//paste this code under the head tag or in a separate js file.
-  // Wait for window load
-  $(window).load(function() {
-    // Animate loader off screen
-    $(".se-pre-con").fadeOut(2500);;
-  });
 $(document).ready(function (){
   /* Slider properties */
   $('.bxslider').bxSlider({
