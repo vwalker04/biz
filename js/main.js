e9da959804f7e73ed81d67d09a9a21dbc78a22d7
Vaughn <vwalker04@yahoo.com>
2017-12-01 22:43:53 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,15 +1,29 @@
 $(document).ready(function (){
   /* Slider properties */
   $('.bxslider').bxSlider({
+    mode: 'fade',
     auto: true,
+    autoHover: true,
     pause: 4000,
-    slideMargin: 20
+    slideMargin: 0
   });
 
-  /* About-container image brighten effect */
+  /* jQuery - About images brighten effect */
   $('.about_img').hover(function() {
     $(this).css("opacity", "1");
     }, function() {
     $(this).css("opacity", ".80");
   });
+
+  /* Webpage pre-loader function */
+  $(window).on("load", function(){
+    $('.loader').fadeOut(4000, function() {
+      $(this).remove();
+    });
+  });
+
 });
+/* Forces user to load page with https if not http */
+if (location.protocol != 'https:') {
+  // location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+}
