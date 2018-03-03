df7fe55fe79b19382629b62977f5e931de531366
vwalker04 <vwalker04@yahoo.com>
2018-03-03 15:01:56 -0500

    Styled contact, footer and added javascript snippet for current year

--- js/main.js
+++ js/main.js
@@ -1,12 +1,4 @@
 $(document).ready(function (){
-  /* Slider properties */
-  $('.bxslider').bxSlider({
-    mode: 'fade',
-    auto: true,
-    autoHover: true,
-    pause: 4000,
-    slideMargin: 0
-  });
 
   /* jQuery - About images brighten effect */
   $('.about_img').hover(function() {
@@ -15,15 +7,9 @@ $(document).ready(function (){
     $(this).css("opacity", ".80");
   });
 
-  /* Webpage pre-loader function */
-  $(window).on("load", function(){
-    $('.loader').fadeOut(4000, function() {
-      $(this).remove();
-    });
-  });
-
 });
+
 /* Forces user to load page with https if not http */
-  // if (location.protocol != 'https:') {
-  //   location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
-  // }
+  if (location.protocol != 'https:') {
+    location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+  }
