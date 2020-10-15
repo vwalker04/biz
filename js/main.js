2bb361f85ddbd1c2080593f5157f32fbf985a198
Vaughn Walker <vwalker04@yahoo.com>
2020-10-14 22:25:28 -0400

    Add WIP contact form page

--- js/main.js
+++ js/main.js
@@ -1,15 +1,38 @@
-$(document).ready(function (){
+(() => {
+  const form = document.querySelector('form');
+  const formResponse = document.querySelector('js-form-response');
 
-  /* jQuery - About images brighten effect */
-  // $('.about_img').hover(function() {
-  //   $(this).css("opacity", "1");
-  //   }, function() {
-  //   $(this).css("opacity", ".80");
-  // });
+  form.onsubmit = e => {
+      e.preventDefault();
 
-});
+      // Prepare data to send
+      const data = {};
+      const formElements = Array.from(form);
+      formElements.map(input => (data[input.name] = input.value));
 
-/* Forces user to load page with https if not http */
-  // if (location.protocol != 'https:') {
-  //   location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
-  // }
+      // Log what our lambda function will receive
+      console.log(JSON.stringify(data));
+
+      // Construct HTTP request
+      let xhr = new XMLHttpRequest();
+      xhr.open(form.method, form.action, true);
+      xhr.setRequestHeader('Accept', 'application/json; charset=utf-8');
+      xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+
+      // Send collected data as JSON
+      xhr.send(JSON.stringify(data));
+
+      // Callback function
+      xhr.onloadend = response => {
+          if (response.target.status === 200) {
+              // The form submission was successful
+              form.reset();
+              formResponse.innerHTML = 'Thanks for the message. I’ll be in touch shortly.';
+          } else {
+              // The form submission failed
+              formResponse.innerHTML = 'Something went wrong';
+              console.error(JSON.parse(response.target.response).message);
+          }
+      };
+  };
+})();
