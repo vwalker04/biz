dd03ff9bf38f5e0d20186c651df720dc65c09447
Vaughn Walker <vwalker04@yahoo.com>
2020-10-16 21:27:02 -0400

    Sanity check with console logs

--- js/main.js
+++ js/main.js
@@ -1,39 +1,41 @@
 (() => {
-  const form = document.querySelector('form');
-  const formResponse = document.querySelector('js-form-response');
+    const form = document.querySelector('form');
+    const formResponse = document.querySelector('js-form-response');
 
-  form.onsubmit = e => {
-      e.preventDefault();
+    form.onsubmit = e => {
+        e.preventDefault();
 
-      // Prepare data to send
-      const data = {};
-      const formElements = Array.from(form);
-      formElements.map(input => (data[input.name] = input.value));
+        // Prepare data to send
+        const data = {};
+        const formElements = Array.from(form);
+        formElements.map(input => (data[input.name] = input.value));
 
-      // Log what our lambda function will receive
-      console.log(JSON.stringify(data));
+        // Log what our lambda function will receive
+        console.log(JSON.stringify(data));
 
-      // Construct HTTP request
-      let xhr = new XMLHttpRequest();
-      xhr.open(form.method, form.action, true);
-      xhr.setRequestHeader('Accept', 'application/json');
-      xhr.setRequestHeader('Content-Type', 'application/json');
-      xhr.withCredentials = false;
+        // Construct HTTP request
+        let xhr = new XMLHttpRequest();
+        xhr.open(form.method, form.action, true);
+        xhr.setRequestHeader('Accept', 'application/json');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.withCredentials = false;
 
-      // Send collected data as JSON
-      xhr.send(JSON.stringify(data));
+        // Send collected data as JSON
+        xhr.send(JSON.stringify(data));
 
-      // Callback function
-      xhr.onloadend = response => {
-          if (response.target.status === 200) {
-              // The form submission was successful
-              form.reset();
-              formResponse.innerHTML = 'Thanks for the message. I’ll be in touch shortly.';
-          } else {
-              // The form submission failed
-              formResponse.innerHTML = 'Something went wrong';
-              console.error(JSON.parse(response.target.response).message);
-          }
-      };
-  };
+        // Callback function
+        xhr.onloadend = response => {
+            if (response.target.status === 200) {
+                // The form submission was successful
+                console.log("Success")
+                form.reset();
+                formResponse.innerHTML = 'Thanks for the message. I’ll be in touch shortly.';
+            } else {
+                // The form submission failed
+                console.log("Fail")
+                formResponse.innerHTML = 'Something went wrong';
+                console.error(JSON.parse(response.target.response).message);
+            }
+        };
+    };
 })();
