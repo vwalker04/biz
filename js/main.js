315413e845f1e768abc23290c8ee2cfc23307fd9
Vaughn Walker <vwalker04@yahoo.com>
2020-10-25 01:35:15 -0400

    More refactor

--- js/main.js
+++ js/main.js
@@ -5,30 +5,22 @@
     form.onsubmit = e => {
         e.preventDefault();
 
-        // Prepare data to send
-        const data = {};
-        const formElements = Array.from(form);
-        formElements.map(input => (data[input.name] = input.value));
-
-        const requestBody = JSON.stringify(data);
+        const requestBody = buildRequest(form);
         console.log(`Request Body: ${requestBody}`);
 
         let xhr = initializeAsyncRequest(form.method, form.action)
 
-        // Send collected data as JSON
         xhr.send(requestBody);
 
         // Callback function. Take the response data from the send() and do something
         xhr.onloadend = response => {
             if (response.target.status === 200) {
-                // The form submission was successful
                 form.reset();
                 addPadding(responseToUser);
                 responseToUser.innerHTML = "Your message has been successfully sent 🎉. We'll be in touch soon!";
                 fadeOut(responseToUser);
                 console.log("Message successfully sent. 💪🏾")
             } else {
-                // The form submission failed
                 addPadding(responseToUser);
                 responseToUser.innerHTML = "Oops. Something went wrong. Please try again later or <a href=\"mailto:contact@avlabels.com\">email us</a>";
                 console.error(JSON.parse(response.target.response).message);
@@ -45,6 +37,18 @@ function initializeAsyncRequest(method, url) {
     xhr.withCredentials = false;
 
     return xhr;
+}
+
+function buildRequest(form) {
+    const formData = createMapfromFormData(form);
+    return JSON.stringify(formData);
+}
+
+function createMapfromFormData(form) {
+    const data = {};
+    const formElements = Array.from(form);
+    formElements.map(input => (data[input.name] = input.value));
+    return data;
 }
 
 function addPadding(element) {
