7004dd297cc8666420da8c1f6a2bd1a5b7c87c82
Vaughn Walker <vwalker04@yahoo.com>
2022-01-20 21:35:28 -0500

    Inline SES send call within AWSEmailService. Add labels to HTML.

--- serverless-contact-form/AWS/AWSEmailService.js
+++ serverless-contact-form/AWS/AWSEmailService.js
@@ -1,43 +1,43 @@
-const AWSClient = require('./AWSEmailClient');
+const AWS = require('aws-sdk');
+const SES = new AWS.SES();
 
 const adamsEmail = "adam@avlabels.com";
 const vaughnEmail = "vaughn@avlabels.com";
 const sourceEmail = "contact@avlabels.com";
 
 function createSESRequest(data) {
-    const request = {
-        Destination: {
-          CcAddresses: [
-            // Optional CC Addresses here
-          ],
-          ToAddresses: [
-            `${vaughnEmail}`,
-            `${adamsEmail}`
-          ]
-        },
-        Source: `${sourceEmail}`,
-        ReplyToAddresses: [data.reply_to],
-        Message: {
-          Body: {
-            Html: {
-              Charset: "UTF-8",
-              Data: `<b><u>Message</u></b><br/>
+    return {
+          Destination: {
+              CcAddresses: [
+                  // Optional CC Addresses here
+              ],
+              ToAddresses: [
+                  `${vaughnEmail}`,
+                  // `${adamsEmail}`
+              ]
+          },
+          Source: `${sourceEmail}`,
+          ReplyToAddresses: [data.reply_to],
+          Message: {
+              Body: {
+                  Html: {
+                      Charset: "UTF-8",
+                      Data: `<b><u>Message</u></b><br/>
               ${data.message}<br/><br/>
               <b><u>Sender Name:</u></b> ${data.name}<br/>
               <b><u>Sender Email:</u></b> ${data.reply_to}<br/>
               <b><u>Sender Phone No.:</u></b> ${data.phone}<br/>`
-            }
-          },
-          Subject: {
-            Charset: 'UTF-8',
-            Data: '📨 New message from AVLabels.com'
+                  }
+              },
+              Subject: {
+                  Charset: 'UTF-8',
+                  Data: '📨 New message from AVLabels.com'
+              }
           }
-        }
-      }
-      return request;
+      };
 }
 
 module.exports.sendEmail = (data, context) => {
     const request = createSESRequest(data);
-    AWSClient.sendEmail(request, context)
+    SES.sendEmail(request, context.done)
 }
