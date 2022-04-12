65c5ac69ef607997a06cdc8e27b0ad0daaaae697
Vaughn Walker <vwalker04@yahoo.com>
2022-04-11 23:42:28 -0400

    Add personal email addresses to SES after adding SPF record in Google domains

--- serverless-contact-form/AWS/AWSEmailService.js
+++ serverless-contact-form/AWS/AWSEmailService.js
@@ -2,36 +2,43 @@ const AWS = require('aws-sdk');
 const SES = new AWS.SES();
 
 function createSESRequest(data) {
-    console.log(`Sending email to ${process.env.ADAMS_EMAIL} and ${process.env.VAUGHNS_EMAIL}`, data);
+    console.log(`Sending email to 
+    ${process.env.VAUGHN_PERSONAL_EMAIL}, 
+    ${process.env.ADAM_PERSONAL_EMAIL}, 
+    ${process.env.ADAMS_EMAIL}, and 
+    ${process.env.VAUGHNS_EMAIL} with data`, data);
+
     return {
-          Destination: {
-              CcAddresses: [
-                  // Optional CC Addresses here
-              ],
-              ToAddresses: [
-                  `${process.env.VAUGHNS_EMAIL}`,
-                  `${process.env.ADAMS_EMAIL}`
-              ]
-          },
-          Source: `${process.env.CONTACT_EMAIL}`,
-          ReplyToAddresses: [data.reply_to],
-          Message: {
-              Body: {
-                  Html: {
-                      Charset: "UTF-8",
-                      Data: `<b><u>Message</u></b><br/>
+        Destination: {
+            CcAddresses: [
+                // Optional CC Addresses here
+                `${process.env.VAUGHN_PERSONAL_EMAIL}`,
+                `${process.env.ADAM_PERSONAL_EMAIL}`
+            ],
+            ToAddresses: [
+                `${process.env.ADAMS_EMAIL}`,
+                `${process.env.VAUGHNS_EMAIL}`
+            ]
+        },
+        Source: `${process.env.CONTACT_EMAIL}`,
+        ReplyToAddresses: [data.reply_to],
+        Message: {
+            Body: {
+                Html: {
+                    Charset: "UTF-8",
+                    Data: `<b><u>Message</u></b><br/>
               ${data.message}<br/><br/>
               <b><u>Sender Name:</u></b> ${data.name}<br/>
               <b><u>Sender Email:</u></b> ${data.reply_to}<br/>
               <b><u>Sender Phone No.:</u></b> ${data.phone}<br/>`
-                  }
-              },
-              Subject: {
-                  Charset: 'UTF-8',
-                  Data: '📨 New message from AVLabels.com'
-              }
-          }
-      };
+                }
+            },
+            Subject: {
+                Charset: 'UTF-8',
+                Data: '📨 New message from AVLabels.com'
+            }
+        }
+    };
 }
 
 module.exports.sendEmail = (data, context) => {
